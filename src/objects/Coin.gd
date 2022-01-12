cfad24309c24a327861d296f970683240e95814e
ZacAllen <zacharysallen97@gmail.com>
2022-01-12 15:48:00 -0500

    nightman game now

--- src/objects/Coin.gd
+++ src/objects/Coin.gd
@@ -5,5 +5,5 @@ onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 
 
-func _on_body_entered(body):
+func _on_Coin_body_entered(body):
 	anim_player.play("fade_out") #play fadeout anim when colliding with player
